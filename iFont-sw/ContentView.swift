512c21c04198a08c65c711bfa55f9893686d33ce
mumu <690481115@qq.com>
2026-04-14 12:30:15 +0800

    Initial Commit

---
+++ iFont-sw/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iFont-sw
+//
+//  Created by 木木 on 2026/4/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iFont-sw/iFont_swApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFont_swApp.swift
+//  iFont-sw
+//
+//  Created by 木木 on 2026/4/14.
+//
+
+import SwiftUI
+
+@main
+struct iFont_swApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
